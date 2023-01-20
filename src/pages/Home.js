c9b92df54d1464c417460728d8585cd007d19236
Roni <ronniiii@yahoo.com>
2023-01-20 14:57:19 +0100

    about component on home page

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,14 +1,14 @@
 import React from 'react'
 import Hero from '../components/Home/Hero'
 import Work from '../components/Home/Work'
-// import About from '../components/Home/About'
+import About from '../components/Home/About'
 
 function Home() {
     return (
         <>
             <Hero />
             <Work />
-            {/* <About /> */}
+            <About />
         </>
     )
 }
