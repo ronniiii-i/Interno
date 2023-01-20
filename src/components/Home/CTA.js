0dbffc2e035997d41a145dd05bcb4217cfb9efd4
Roni <ronniiii@yahoo.com>
2023-01-21 00:09:48 +0100

    [Update] styled cta on home page

--- src/components/Home/CTA.js
+++ src/components/Home/CTA.js
@@ -2,11 +2,11 @@ import { FaArrowRight } from "react-icons/fa";
 
 function CTA() {
   return (
-    <section id="cta">
-        <div className="inner">
+    <section id="cta" className="flex align-center justify-center">
+        <div className="inner flex column align-center justify-center">
             <h2>Wanna join the interno?</h2>
             <p>It is a long established fact that will be distracted.</p>
-            <a href="">Contact Us <FaArrowRight/></a>
+            <a href="" className="flex align-center justify-between">Contact Us <FaArrowRight/></a>
         </div>
     </section>
   )
