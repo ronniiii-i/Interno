f49e03711b1e356b6ca630665562599978079cc4
Roni <ronniiii@yahoo.com>
2023-02-10 22:11:13 +0000

    [STYLING]Styled header component

--- src/components/Nav.js
+++ src/components/Nav.js
@@ -1,22 +1,33 @@
 import React from 'react'
 import logo from '../assets/imgs/Logo.svg'
+import { BsSearch } from 'react-icons/bs'
+import { MdClose, MdMenu } from 'react-icons/md'
 
 function Nav() {
   return (
-    <nav>
+    <header>
+      <div className='flex align-center justify-between'>
+        <MdMenu className='burger' />
         <div className="logo">
-            <img src={logo} alt="Interno" />
+          <img src={logo} alt="Interno" />
         </div>
-        <ul>
+        <nav>
+          <MdClose className='close' />
+          <div className="logo">
+            <img src={logo} alt="Interno" />
+          </div>
+          <ul>
             <li><a href="/">Home</a></li>
             <li><a href="">Pages</a></li>
             <li><a href="">Services</a></li>
             <li><a href="">Project</a></li>
             <li><a href="">Blog</a></li>
-            <li><a href="">Conntact</a></li>
-            <li><a href=""></a></li>
-        </ul>
-    </nav>
+            <li><a href="">Contact</a></li>
+            <li><a href=""><BsSearch /></a></li>
+          </ul>
+        </nav>
+      </div>
+    </header>
   )
 }
 
