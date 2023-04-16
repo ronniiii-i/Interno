c122a4e88ddf9f9b2562c7abbc934c4743f6489b
Roni <ronniiii@yahoo.com>
2023-04-17 00:58:55 +0100

    [STYLING]styled 404 page

--- src/components/NotFound.js
+++ src/components/NotFound.js
@@ -3,7 +3,12 @@ import React from 'react'
 function NotFound() {
   return (
     <main>
-        <h1>404</h1>
+      <div className="bg full-width">
+        <h1 className='big m-auto t-center'>404</h1>
+        <h1 className="t-center">Page Not Found</h1>
+        <h4 className="t-center">It appears that you have put in an incorrect url</h4>
+        <h4 className="t-center">Click <a className='underline h4' href="/">here</a> to go back home</h4>
+        </div>
     </main>
   )
 }
